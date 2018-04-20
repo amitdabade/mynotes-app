ba7932f945a80903bdcb60156878edeb0ccc0c3a
Amit Dabade <31438237+amitdabade@users.noreply.github.com>
2018-04-20 17:59:21 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var api = express.Router();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
