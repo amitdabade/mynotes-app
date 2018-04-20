4bad90205c218e68794b20c290e028cd57ba764b
Amit Dabade <31438237+amitdabade@users.noreply.github.com>
2018-04-20 16:46:30 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
 
-app.get("/", function(req, res){
-    res.send("welcome!!");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'sql12.freemysqlhosting.net',
+  user     : 'sql12233718',
+  password : 'z3nKLC9K28',
+  database : 'sql12233718'
 });
 
+connection.connect();
+
+app.get("/", function(req, res){
+    connection.query('SELECT * FROM users WHERE user_email="amit@amit.com"', function (err, rows, fields) {
+        if (err) throw err
+      
+        res.send("welcome!! "+ rows[0].user_email);
+      });
+      
+    connection.end();
+    // res.send("welcome!!");
+    
+});
+process.on('uncaughtException', function (err) {
+    console.log(err);
+});
+var port = process.env.PORT || 3000;
 app.listen(port);
